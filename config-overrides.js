2324f694a999fc04dd9d09d8c4b6ebf9dedf64b1
Ivan Ortega <ivanortegaalba@gmail.com>
2018-01-31 09:07:14 +0100

    Update

--- config-overrides.js
+++ config-overrides.js
@@ -3,7 +3,9 @@ const rewireStyledComponents = require('react-app-rewire-styled-components')
 module.exports = function override (config, env) {
   // add a plugin
   config = rewireStyledComponents(config, env, {
-    displayName: true
+    displayName: true,
+    preprocess: true,
+    minify: false,
   })
   return config
 }
